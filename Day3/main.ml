4633c74d772f6652441c7f8d0f2a351187222be9
Tatiana Racheva <tatianaracheva@Tatianas-MacBook-Air.local>
2020-12-02 23:33:27 -0800

    Day 3 solutions

--- Day3/main.ml
+++ Day3/main.ml
@@ -3,73 +3,52 @@ open Core_kernel
 let log x =
   Printf.printf (x ^^ "\n%!")
 
-type policy = {
-  letter: char;
-  min: int;
-  max: int;
-} [@@deriving show]
-
-type t = {
-  policy: policy;
-  password: string;
-} [@@deriving show]
-
-let make line : t =
-  let line_channel = Scanf.Scanning.from_string line in
-  let f min max letter password = {
-    policy = {
-      letter;
-      min;
-      max
-    };
-    password;
-  }
-  in
-  Scanf.bscanf line_channel "%d-%d %c: %s" f
-
-let read_input filename : t list =
+let read_input filename : (char array) array =
   let file = In_channel.create filename in
   let lines = In_channel.input_lines file in
   In_channel.close file;
-  List.map lines ~f:make
+  List.map lines ~f:(fun line -> String.to_array line)
+  |> List.to_array
 
-let valid_part1 (t: t) : int =
-  let chars = String.to_list t.password in
-  let count = List.count chars ~f:(fun (c: char) -> Char.(=) c t.policy.letter) in
-  if count >= t.policy.min && count <= t.policy.max then
-    1
+let rec step
+    ~count
+    ~row
+    ~col
+    ~slope
+    (a: (char array) array) : int =
+  if row >= Array.length a then
+    count
   else
-    0
-
-let valid_part2 (t: t) : int =
-  let chars = String.to_array t.password in
-  let length = Array.length chars in
-  let first = t.policy.min - 1 in
-  let last = t.policy.max - 1 in
-  let c = t.policy.letter in
-  if first >= length then
-    0
-  else if last >= length then
-    if Char.(=) chars.(first) c then
-      1
-    else
-      0
-  else
-    if (Char.(=) chars.(first) c && Char.(<>) chars.(last) c) ||
-       (Char.(=) chars.(last) c && Char.(<>) chars.(first) c) then
-      1
-    else
-      0
-
-let count_valid_passwords l valid : int =
-  List.fold l ~init:0 ~f:(fun acc t -> acc + (valid t))
+    let (row_incr, col_incr) = slope in
+    let width = Array.length a.(row) in
+    let count = if Char.(=) a.(row).(col) '#' then 1 + count else count in
+    let row = row + row_incr in
+    let col = (col + col_incr) mod width in
+    step ~count ~row ~col ~slope a
 
 (* To run: `dune exec ./main.exe -- ./input` *)
 let () =
   let input = Sys.argv.(1) in
-  log "Filename: %s\n%!" input;
-  let l = read_input input in
-  let count = count_valid_passwords l valid_part1 in
+  log "Filename: %s" input;
+
+  let trees = read_input input in
+  let count = step ~count:0 ~row:0 ~col:0 ~slope:(1, 3) trees in
   log "Part 1: %d" count;
-  let count = count_valid_passwords l valid_part2 in
-  log "Part 2: %d" count
+
+  let slopes = [
+    (1, 1);
+    (1, 3);
+    (1, 5);
+    (1, 7);
+    (2, 1);
+  ]
+  in
+  let product =
+    List.fold
+      slopes
+      ~init:1
+      ~f:(fun product slope ->
+        log "Slope (%d, %d)" (fst slope) (snd slope);
+        product * step ~count:0 ~row:0 ~col:0 ~slope trees)
+  in
+  log "Part 2: %d" product
