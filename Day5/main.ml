f675df11131014002032d148e4b741fb6ebb67b8
Tatiana Racheva <tatianaracheva@Tatianas-MacBook-Air.local>
2020-12-03 23:35:31 -0800

    Day5 stubs

---
+++ Day5/main.ml
@@ -0,0 +1,21 @@
+open Core_kernel
+
+let log x =
+  Printf.printf (x ^^ "\n%!")
+
+let read_input filename : string list =
+  let file = In_channel.create filename in
+  let lines = In_channel.input_lines file in
+  In_channel.close file;
+  lines
+
+(*
+  dune exec ./main.exe -- ./small_input
+  dune exec ./main.exe -- ./input
+*)
+let () =
+  let input = Sys.argv.(1) in
+  log "Filename: %s" input;
+
+  let lines = read_input input in
+  ignore lines
